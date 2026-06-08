687adf0fd366ce4f4a01b43c2079c816cb91be0a
CyberCodeCracker <souhailamouri123@gmail.com>
2026-06-09 00:01:40 +0100

    a more optimized contains duplicates

--- array/src/main/java/org/amouri/ContainsDuplicates.java
+++ array/src/main/java/org/amouri/ContainsDuplicates.java
@@ -7,10 +7,13 @@ public class ContainsDuplicates {
 
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) return true;
-            set.add(nums[i]);
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
         }
+
         return false;
     }
 }
