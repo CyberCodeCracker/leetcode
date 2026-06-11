daa5d00472bba392d2e225ba9b216d6cae430893
CyberCodeCracker <souhailamouri123@gmail.com>
2026-06-12 00:54:33 +0100

    Top K Frequent

---
+++ array/src/main/java/org/amouri/KFrequent.java
@@ -0,0 +1,42 @@
+package org.amouri;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KFrequent {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int[] res = new int[map.size()];
+        int index = 0;
+
+        for (int freq : map.values()) {
+            res[index++] = freq;
+        }
+
+        Arrays.sort(res);
+
+        int threshold = res[res.length - k];
+
+        int[] result = new int[k];
+        index = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= threshold) {
+                result[index++] = entry.getKey();
+
+                if (index == k) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
