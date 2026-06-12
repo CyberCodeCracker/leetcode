4da27cdbf335743890c7915fbb483488c22258c2
CyberCodeCracker <souhailamouri123@gmail.com>
2026-06-12 13:30:56 +0100

    verify alien dictionary

---
+++ array/src/main/java/org/amouri/AlienSorted.java
@@ -0,0 +1,34 @@
+package org.amouri;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlienSorted {
+
+    public boolean isAlienSorted(String[] words, String order) {
+        Map<Character, Integer> orderMap = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < order.length(); i++) {
+            orderMap.put(order.charAt(i), i);
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                if (j >= words[i + 1].length()) {
+                    return false;
+                }
+                if (words[i].charAt(j) != words[i + 1].charAt(j)) {
+                    int currentLetter = orderMap.get(words[i].charAt(j));
+                    int nextLetter = orderMap.get(words[i + 1].charAt(j));
+                    if (nextLetter < currentLetter) {
+                        return false;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
