1c344ea7bd80559edecb85f262d456a2cd224a21
CyberCodeCracker <souhailamouri123@gmail.com>
2026-06-14 01:59:19 +0100

    Best time to buy

---
+++ array/src/main/java/org/amouri/BestTimeToBuy.java
@@ -0,0 +1,18 @@
+package org.amouri;
+
+public class BestTimeToBuy {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0)
+            return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            int currentProfit = prices[i] - minPrice;
+            maxProfit = Math.max(currentProfit, maxProfit);
+        }
+        return maxProfit;
+    }
+}
