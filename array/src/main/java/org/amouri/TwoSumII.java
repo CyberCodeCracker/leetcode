f3ebb4cf5f108c3c3fe51bbc8cb703ba54fb767d
CyberCodeCracker <souhailamouri123@gmail.com>
2026-06-18 13:10:04 +0100

    Two sum II

---
+++ array/src/main/java/org/amouri/TwoSumII.java
@@ -0,0 +1,24 @@
+package org.amouri;
+
+public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left++, right++};
+            }
+            if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return new int[0];
+    }
+
+}
